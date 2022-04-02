5d112a6c9ef40d36c1acbad149e43b925a2885a5
Tomato6966 <chris.pre03@gmail.com>
2022-04-02 16:17:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 const { Client, Intents, Collection, MessageEmbed } = require('discord.js'); //v13
 const { getVoiceConnection } = require("@discordjs/voice");
-/**
- * TODO:
- * speed Command
- * bassboost Command
- */
-const client = new Client({
-    shards: "auto",
-    allowedMentions: {
-      parse: ["roles", "users", /* "everyone" */],
-      repliedUser: false, //set true if you want to ping the bot on reply messages
-    },
-    failIfNotExists: false,
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-    intents: [ 
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_VOICE_STATES
-    ]
-});
+
+const client = new Client(clientSettingsObject());
+// Slash Commands deployment settings
+client.deploySlash = {
+  enabled: true,
+  guild: false, // false | "ID" (if it's false, just set global once, and then never needed again!)
+}
 // Global config file
 client.config = require("./config/config.json");
 
@@ -34,3 +22,20 @@ require("./util/musicUtils.js")(client);
 require("./util/handler.js")(client);
 
 client.login(client.config.token);
+
+function clientSettingsObject() {
+  return {
+    shards: "auto",
+    allowedMentions: {
+      parse: ["roles", "users", /* "everyone" */],
+      repliedUser: false, //set true if you want to ping the bot on reply messages
+    },
+    failIfNotExists: false,
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+    intents: [ 
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES
+    ]
+}
+}
