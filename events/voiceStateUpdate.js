0a079c579c6af2332382d29f52d8d0e4843b0ed5
Tomato6966 <chris.pre03@gmail.com>
2022-04-03 21:39:03 +0200

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -2,13 +2,10 @@ const { Permissions } = require("discord.js");
 const { getVoiceConnection } = require("@discordjs/voice")
 module.exports = async (client, oldState, newState) => {
     // Stage Channel self suspress
-    if (newState.id == client.user.id && newState.channelId && newState.channel.type == "GUILD_STAGE_VOICE" && newState.guild.me.voice.suppress) {
-        try {
-            if(newState.channel?.permissionsFor(newState.guild?.me)?.has(Permissions.FLAGS.MUTE_MEMBERS))
-                await newState.guild.me.voice.setSuppressed(false).catch(console.warn);
-        } catch (e) {
-            console.log(e)
-        }
+    if(newState.id == client.user.id && newState.channelId && newState.channel.type == "GUILD_STAGE_VOICE" && newState.suppress) {
+        if(newState.channel?.permissionsFor(newState.guild.me)?.has(Permissions.FLAGS.MUTE_MEMBERS)) {
+            await newState.guild.me.voice.setSuppressed(false).catch(() => null);
+        } 
     }
     
     if(newState.id == client.user.id) return;
@@ -18,26 +15,20 @@ module.exports = async (client, oldState, newState) => {
         if(one === false && two === true || one === true && two === false) return true;
         else return false;
     }
-    const o = oldState, n = newState; 
-    if (stateChange(o.streaming, n.streaming) || 
-      stateChange(o.serverDeaf, n.serverDeaf) || 
-      stateChange(o.serverMute, n.serverMute) || 
-      stateChange(o.selfDeaf, n.selfDeaf) || 
-      stateChange(o.selfMute, n.selfMute) || 
-      stateChange(o.selfVideo, n.selfVideo)) return;
-    // new Channel joined
-    if (!o.channelId && n.channelId) {
+    const o = oldState, n = newState;
+    if(stateChange(o.streaming, n.streaming) ||
+      stateChange(o.serverDeaf, n.serverDeaf) ||
+      stateChange(o.serverMute, n.serverMute) ||
+      stateChange(o.selfDeaf, n.selfDeaf) ||
+      stateChange(o.selfMute, n.selfMute) ||
+      stateChange(o.selfVideo, n.selfVideo) ||
+      stateChange(o.suppress, n.suppress)) return;
+    // channel joins
+    if(!o.channelId && n.channelId) {
         return;
     }
-    // channel left
-    if (o.channelId && !n.channelId) {
-        const connection = getVoiceConnection(n.guild.id);
-        if(o.channel.members.filter(m => !m.user.bot && !m.voice.selfDeaf && !m.voice.serverDeaf).size >= 1) return;
-        if(connection && connection.joinConfig.channelId == o.channelId) connection.destroy();
-        return;
-    }
-    // channel swapped - register a LEAVE
-    if (o.channelId && n.channelId) {
+    // channel leaves
+    if(!n.channelId && o.channelId || n.channelId && o.channelId) {
         const connection = getVoiceConnection(n.guild.id);
         if(o.channel.members.filter(m => !m.user.bot && !m.voice.selfDeaf && !m.voice.serverDeaf).size >= 1) return;
         if(connection && connection.joinConfig.channelId == o.channelId) connection.destroy();
